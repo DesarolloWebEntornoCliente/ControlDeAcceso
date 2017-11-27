c020fabc0472d85c160e57752330cca3830a4d4d
Railton Cardoso <railton.cardoso@hotmail.com>
2017-11-27 07:06:25 +0100

    casi todo hecho

---
+++ ControleDeAcceso/src/main/java/es/altair/hibernate/dao/CodigoAccesoDAO.java
@@ -0,0 +1,14 @@
+package es.altair.hibernate.dao;
+
+import java.util.List;
+
+import es.altair.hibernate.bean.CodigoAcceso;
+
+public interface CodigoAccesoDAO {
+
+	public void save(CodigoAcceso cod);
+
+	public List<Object[]> listaCodigosAcceso();
+
+	public void listaCodigosDesincriptados();
+}
---
+++ ControleDeAcceso/src/main/java/es/altair/hibernate/dao/CodigoAccesoDAOImplHIbernate.java
@@ -0,0 +1,75 @@
+package es.altair.hibernate.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import es.altair.hibernate.bean.CodigoAcceso;
+import es.altair.hibernate.utilitarios.Encriptaciones;
+
+public class CodigoAccesoDAOImplHIbernate implements CodigoAccesoDAO {
+
+	public void save(CodigoAcceso cod) {
+		
+	Session sesion = Conexion.abrirConexion();
+		
+		sesion.save(cod);
+		sesion.getTransaction().commit();
+				
+	Conexion.desconectar(sesion);
+		
+	}
+
+	public List<Object[]> listaCodigosAcceso() {
+		
+		List<Object[]> codigos = new ArrayList<Object[]>();
+		
+			Session sesion = Conexion.abrirConexion();
+				
+				codigos = sesion.createQuery("select e.idCodigoAcceso, e.nivel from CodigoAcceso e").list(); // Consulta HQL
+			
+				sesion.getTransaction().commit();
+						
+			Conexion.desconectar(sesion);
+		
+		return codigos;
+	}
+
+	public void listaCodigosDesincriptados() {
+		
+		 String key = "92AE31A79FEEB2A3"; //llave
+		 String iv = "0123456789ABCDEF"; // vector de inicialización
+			 
+		 List<Object[]> codigos = new ArrayList<Object[]>();
+		
+		 Session sesion = Conexion.abrirConexion();
+			
+			codigos = sesion.createQuery("select e.idCodigoAcceso, e.nivel, e.codigoAcceso from CodigoAcceso e").list();
+			
+			System.out.println("Lista de Códigos de Acceso");
+			System.out.println("═════════════════════════════════════════════════════════════");
+			//System.out.println("\tCód.\tNIvel de Acceso");
+			System.out.println();
+			for (Object[] objects : codigos) {
+				 try {
+				 		//Codigo de desincriptacion //	 
+						String nombreOriginal =  Encriptaciones.decrypt(key, iv,objects[2].toString());
+						String.format("%-10s", objects[1].toString());
+						System.out.println("\t"+objects[0] + "  " + String.format("%-20s", objects[1].toString()) + "\t" + String.format("%-15s", nombreOriginal) + "\t" + objects[2].toString()); 
+
+					} catch (Exception e) {
+						System.out.println("No ha sido posible desincriptar ese codigo");;
+				}
+	
+			}
+		
+			sesion.getTransaction().commit();
+					
+		Conexion.desconectar(sesion);
+		
+			 
+		
+	}
+
+}
